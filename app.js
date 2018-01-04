916635f18b08be8d7a634efbed8f6601344ac094
Courtney Severson <courtneyseverson8@gmail.com>
2018-01-03 20:22:21 -0600

    added additional functions

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
-/*
-Build all of your functions for displaying and gathering information below (GUI).
-*/
-
-// app is the function called to start the entire application
 function app(people){
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch(searchType){
     case 'yes':
-    // TODO: search by name
-    var foundPerson = searchByName(people);
+      var foundPerson = searchByName(people);
+      mainMenu();
+      //need to return the person name - is this correct?
     break;
     case 'no':
-    // TODO: search by traits
+    var foundPerson = height(people);
+    foundPerson = weight(foundPerson);
+    foundPerson = age(foundPerson);
+    foundPerson = occupation(foundPerson);
+    foundPerson = eyeColor(foundPerson);
+    return(foundPerson);
     break;
     default:
-    app(people); // restart app
+    app(people);
     break;
   }
-}
+} 
 
-// Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
   if(!person){
     alert("Could not find that individual.");
-    return app(people); // restart
+    return app(people); 
   }
 
   var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
   switch(displayOption){
     case "info":
-    // TODO: get person's info
+      var info = displayPerson(person);
+      mainMenu(person, people);
+      alert(info);
+      return info;
+      mainMenu(person, people);
     break;
     case "family":
-    // TODO: get person's family
+      prompt("")
+      mainMenu(person, people);
+      //would need to get a new function
     break;
     case "descendants":
-    // TODO: get person's descendants
+      //need to use recursion - just limit to by blood relatives
+      // var descendants = 
+      // return descendants;
+      mainMenu(person, people);
     break;
     case "restart":
     app(people); // restart
@@ -51,6 +58,122 @@ function mainMenu(person, people){
   }
 }
 
+function family(){
+  var family = people.filter(function(tempPerson){
+  if(person.id === tempPerson.currentSpouse || person.parents[0] === person.parents[0] || person.parents[1] === person.parents[1] || 
+  //need to find siblings and children
+  return true;
+  }
+}
+
+//could try to use .filter
+function ageBirthday(){
+  filter(age);
+  //changes the age to the birthday
+}
+
+function height(person, people){
+  var userInput = prompt("Do you know height?");
+    if(userInput === "yes"){
+    var height = promptFor("What is the person's height in inches?", chars);
+    var foundPerson = people.filter(function(person){
+      if (person.height === height){
+        return true;
+      }
+      else{
+        return false;
+      }
+    })
+    return foundPerson;
+  }
+  else if(userInput === "no"){
+    return;
+  }
+  return foundPerson;
+}
+
+function weight(person, people){
+  var userInput = prompt("Do you know weight?");
+    if(userInput === "yes"){
+    var weight = promptFor("What is the person's weight in pounds?", chars);
+    var foundWeight = people.filter(function(person){
+      if (person.weight === weight){
+        return true;
+      }
+    else{
+      return false;
+      }
+    })
+    return foundPerson;
+    }
+    else if(userInput === "no"){
+      return;
+    }
+    return foundPerson;
+}
+
+
+function age(person, people){
+  var userInput = prompt("Do you know age?");
+    if(userInput === "yes"){
+    var age = promptFor("What is the person's age in years?", chars);
+    var foundAge = people.filter(function(person){
+      if (person.age === age){
+        return true;
+        }
+      else{
+        return false;
+      }
+    })
+    return foundPerson;
+    }
+    elseif(userInput === "no"){
+      return;
+    }
+    return foundPerson;
+}
+
+function occupation(person, people){
+  var userInput = prompt("Do you know occupation?");
+    if (userInput === "yes"){
+    var occupation = promptFor("What is the person's occupation?", chars);
+    var foundOccupation = people.filter(function(person){
+      if (person.occupation === occupation){
+        return true;
+      }
+    else{
+      return false;
+      }
+    })
+    return foundPerson;
+    }
+    elseif(userInput === "no"){
+      return;
+    }
+    return foundPerson;
+}
+
+function eyeColor(person, people){
+  var userInput = prompt("Do you know their eye color?")
+    if (userInput === "yes"){
+    var eyeColor = promptFor("What is the person's eye color?", chars);
+    var foundEyeColor = people.filter(function(person){
+      if (person.EyeColor === EyeColor){
+        return true;
+      }
+    else{
+      return false;
+      }
+    })
+    return foundPerson;
+    }
+    elseif(userInput === "no"){
+      return;
+    }
+    return foundPerson;
+}
+
+// TODO: find the person using the name they entered
 function searchByName(people){
   var firstName = promptFor("What is the person's first name?", chars);
   var lastName = promptFor("What is the person's last name?", chars);
@@ -63,11 +186,10 @@ function searchByName(people){
       return false;
     }
   })
-  // TODO: find the person using the name they entered
-  return foundPerson;
+  var personName = foundPerson[0];
+  return personName;
 }
 
-// alerts a list of people
 function displayPeople(people){
   alert(people.map(function(person){
     return person.firstName + " " + person.lastName;
@@ -75,15 +197,16 @@ function displayPeople(people){
 }
 
 function displayPerson(person){
-  // print all of the information about a person:
-  // height, weight, age, name, occupation, eye color.
   var personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
+  personInfo += "Height" + height + "\n";
+  personInfo += "Weight" + weight + "\n";
+  personInfo += "Age" + dob + "\n";
+  personInfo += "Occupation" + occupation + "\n";
+  personInfo += "Eye color" + eyeColor + "\n";
   alert(personInfo);
 }
 
-// function that prompts and validates user input
 function promptFor(question, valid){
   do{
     var response = prompt(question).trim();
@@ -91,12 +214,10 @@ function promptFor(question, valid){
   return response;
 }
 
-// helper function to pass into promptFor to validate yes/no answers
 function yesNo(input){
   return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
 
-// helper function to pass in as default promptFor validation
 function chars(input){
-  return true; // default validation only
+  return true; 
 }
